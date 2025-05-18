0036adcb700b1ca4801dd90c096c0d836b8f4910
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-18 15:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 //your JS code here. If required.
 const alterTags=()=>{
 	const status=document.getElementById("status");
-	if(status.parentNode){
-		status.parentNode.removeChild(status)
-	}
 	const newHead=document.createElement("h1");
 	newHead.textContent="Entered Metaverse";
 	newHead.setAttribute("id","status");
-	document.body.prepend(newHead)
+	status.replaceWith(newHead)
 }
