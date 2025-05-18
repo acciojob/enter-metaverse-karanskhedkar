4049e7b123eb526d74ce819c062b47dec8837d6a
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-18 15:31:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //your JS code here. If required.
 const alterTags=()=>{
 	const status=document.getElementById("status");
-	const newHead=document.createElement("h1");
-	newHead.textContent="Entered Metaverse";
-	newHead.setAttribute("id","status");
-	status.replaceWith(newHead)
+	if(status.textContent==="Entered Metaverse"){
+		return;
+	}else{
+		const newHead=document.createElement("h1");
+		newHead.textContent="Entered Metaverse";
+		newHead.setAttribute("id","status");
+		status.replaceWith(newHead)
+	}
 }
