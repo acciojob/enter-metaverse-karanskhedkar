b22dc1837aca5d9ef1873bc15f23033235363a9f
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-18 15:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const alterTags=()=>{
 	const status=document.getElementById("status");
 	const newHead=document.createElement("h1");
 	newHead.textContent="Entered Metaverse";
-	// newHead.setAttribute("id","status");
+	newHead.setAttribute("id","status");
 	status.replaceWith(newHead)
 }
